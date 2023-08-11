ec1ac97b8529cb76c35d70a13a246e5246eb434d
KmiloPena <125616941+KmiloPena@users.noreply.github.com>
2023-08-11 19:44:22 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#theExcuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["friend", "python", "Alien", "cat", "pirate", "kraken"];
+  let action = [
+    "destroyed my",
+    "dissapeared my",
+    "took my",
+    "stolen my",
+    "threw my",
+    "ate my"
+  ];
+  let possetion = ["homework", "bike", "car", "house"];
+  let where = ["in the sea", "in my backyard", "in the woods"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let possetionIndx = Math.floor(Math.random() * possetion.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    possetion[possetionIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
